c89e409d47417e8992f73bd1d8b6ca8075447cbe
raulortegal <rortega@interside.cl>
2023-11-26 15:12:36 -0300

    se crea repositorio para desafio banco

---
+++ src/main/java/com/desafio/desafiobanco/DesafiobancoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.desafiobanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/desafiobanco/DesafiobancoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.desafiobanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
